4e639a8f717626a1c77538360832656213eb5759
Steven Chou <steven@swap.ee>
2025-02-04 14:41:02 +0800

    Project init

---
+++ TravelTaipei/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  TravelTaipei
+//
+//  Created by Steven on 2025/2/4.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .lightGray
+    }
+}
